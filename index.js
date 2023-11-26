68fe83b7eabe79042bdd40d4f4cd40d86e831913
IsabelleVolaco <isabelletvolaco@gmail.com>
2023-11-26 19:19:55 -0300

    meus colaboradores pronto

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const btnLogin = document.querySelector("#btn-login");
 btnLogin.addEventListener(`click`);
 
+function openModal() {
+    let modal = document.getElementById('modal');
+    let overlay = document.querySelector('.modal-overlay');
+
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+function closeModal() {
+    let modal = document.getElementById('modal');
+    let overlay = document.querySelector('.modal-overlay');
+
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
