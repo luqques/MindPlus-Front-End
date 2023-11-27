2d666190a54599dfb63f91976c7c63a44c471c89
ISABELLE TSCHOEKE VOLACO <isabelle.volaco@alunos.sc.senac.br>
2023-11-27 19:17:55 -0300

    terminei  as metas

--- index.js
+++ index.js
@@ -22,7 +22,23 @@ function closeModal() {
 // Gráfico Isa Metas 1
 
 function drawChart() {
-    let data = google.visualization.arrayToDataTable([
+    let data1 = google.visualization.arrayToDataTable([
+        ['MÊS', 'Nº Preenchimentos', 'Nº Colaboradores'],
+        ['Jan', 1740, 1996],
+        ['Fev', 1881, 1992],
+        ['Mar', 1866, 2000],
+        ['Abr', 1868, 2000],
+        ['Mai', 1859, 1998],
+        ['Jun', 1980, 1998],
+        ['Jul', 1899, 2004],
+        ['Ago', 1992, 2003],
+        ['Set', 1980, 2000],
+        ['Out', 1981, 2000],
+        ['Nov', 0, 0],
+        ['Dez', 0, 0]
+    ]);
+
+    let data2 = google.visualization.arrayToDataTable([
         ['ANO', 'Nº Preenchimentos', 'Nº Colaboradores'],
         ['2019', 832, 845],
         ['2020', 798, 860],
@@ -30,15 +46,26 @@ function drawChart() {
         ['2022', 1242, 1800]
     ]);
 
-    let options = {
+    let options1 = {
         chart: {
-            title: 'Adesão dos colaboradores por ano (2022).',
-            subtitle: 'Nº de preenchimentos X Nº de Colaboradores. Dados coletados mensalmente: 2019-2022',
+            title: 'Adesão dos colaboradores por mês (2023).',
+            subtitle: 'Nº de Preenchimentos X Nº de Colaboradores. Dados coletados mensalmente.',
         }
     };
 
-    let chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+    let options2 = {
+        chart: {
+            title: 'Adesão dos colaboradores por ano (2019 - 2022).',
+            subtitle: 'Nº de Preenchimentos X Nº de Colaboradores anuais. Dados coletados mensalmente: 2019-2022',
+        }
+    };
 
-    chart.draw(data, google.charts.Bar.convertOptions(options));
+    let chart1 = new google.charts.Bar(document.getElementById('columnchart_material1'));
+    let chart2 = new google.charts.Bar(document.getElementById('columnchart_material2'));
+
+    chart1.draw(data1, google.charts.Bar.convertOptions(options1));
+    chart2.draw(data2, google.charts.Bar.convertOptions(options2));
 }
 
+
+    
