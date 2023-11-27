0317561343df3330a303f66edfa470041ba0d402
IsabelleVolaco <isabelletvolaco@gmail.com>
2023-11-27 17:29:11 -0300

    metas + gráficos e js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Modal Isa - Meus collab
+
 const btnLogin = document.querySelector("#btn-login");
 btnLogin.addEventListener(`click`);
 
@@ -16,3 +18,27 @@ function closeModal() {
     modal.style.display = 'none';
     overlay.style.display = 'none';
 }
+
+// Gráfico Isa Metas 1
+
+function drawChart() {
+    let data = google.visualization.arrayToDataTable([
+        ['ANO', 'Nº Preenchimentos', 'Nº Colaboradores'],
+        ['2019', 832, 845],
+        ['2020', 798, 860],
+        ['2021', 980, 1120],
+        ['2022', 1242, 1800]
+    ]);
+
+    let options = {
+        chart: {
+            title: 'Adesão dos colaboradores por ano (2022).',
+            subtitle: 'Nº de preenchimentos X Nº de Colaboradores. Dados coletados mensalmente: 2019-2022',
+        }
+    };
+
+    let chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
