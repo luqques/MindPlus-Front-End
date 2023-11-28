ebd8d5f99eea18f0b315326e41591488d4cdf5df
Lucas da Cunha <lucas.cunha@ailos.coop.br>
2023-11-28 13:31:47 -0300

    Ajustes de links do javascript, e junção dos dois arquivos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 
+// Js para responsividade da Header
+
+let openMenuLateral = document.querySelectorAll("#open-menu-lateral");
+for (let open of openMenuLateral) {
+  open.addEventListener(`click`, showMenuLateral);
+}
+
+function showMenuLateral() {
+  let menuLateral = document.querySelector("#menu-lateral");
+  menuLateral.classList.add(`menu-lateral-ativo`);
+}
+
+let closeMenuLateral = document.querySelectorAll("#close-menu-lateral");
+for (let close of closeMenuLateral) {
+  close.addEventListener(`click`, hiddenMenuLateral)
+}
+
+function hiddenMenuLateral() {
+  let menuLateral = document.querySelector("#menu-lateral");
+  menuLateral.classList.remove(`menu-lateral-ativo`);
+}
+
 //Modal Isa - Meus collab - my-employees
 
 function openModal() {
