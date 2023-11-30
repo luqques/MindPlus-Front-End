97858b24d1cf4b5506f3b14b4cb5206c3f7e819d
luqques <luques.cunha@gmail.com>
2023-11-29 21:26:59 -0300

    Adicionado função de validação e de formação do JSON para a tela de login

--- src/login/login.js
+++ src/login/login.js
@@ -1,27 +1,25 @@
+$(() => {
 
-var loggedText = document.getElementById('keep-logged-in');
-var loggedCheckbox = document.getElementById('checkbox-keep-logged');
-loggedText.addEventListener('click', function() {
-    loggedCheckbox.checked = !loggedCheckbox.checked;
-});
-loggedCheckbox.addEventListener('click', function() {
-    loggedCheckbox.checked = !loggedCheckbox.checked;
-});
+  $(".btn-entrar").click(() => {
+    const values = {
+      email: $("#email")[0].value,
+      senha: $("#senha")[0].value,
+      materLogado: $("#checkbox-keep-logged")[0].value
+    }
 
-const btnLogin = document.querySelector("#btn-login");
-btnLogin.addEventListener('click', () => {
-  const email = document.querySelector("#email");
-  const senha = document.querySelector("#senha");
+    if (!values.email) {
+      alert("E-mail não informado!");
+      $("#email").addClass("invalid");
+      return;
+    }
+    $("#email").removeClass("invalid");
 
-  if (!email.value) {
-    email.classList.add('invalid');
-  } else {
-    email.classList.remove('invalid');
-  }
+    if (!values.senha) {
+      alert("Senha não informada!");
+      $("#senha").addClass("invalid");
+      return;
+    }
+    $("#senha").removeClass("invalid");
 
-  if (!senha.value) {
-    senha.classList.add('invalid');
-  } else {
-    senha.classList.remove('invalid');
-  }
+  });
 });
