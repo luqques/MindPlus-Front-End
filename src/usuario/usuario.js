7ec5ddd438b23059436b20a277737695f1b253f8
paola.kloch <kloch.157@gmail.com>
2023-12-04 19:09:14 -0300

    Jquery finalizado e funcionando

--- src/usuario/usuario.js
+++ src/usuario/usuario.js
@@ -1,30 +1,21 @@
-$(document).ready(function() {
+$(() => {
 
-    // Função para sallet informações
-    function salletInformacoes() {
-        // Recuperar os valores dos campos
-        let email = $("#email input").val();
-        let contato = $("#contato input").val();
-        let endereco = $("#endereço input").val();
-        let cargo = $("#cargo input").val();
-        let modeloContratacao = $("#modelo-de-contratação input").val();
-        let minhaEmpresa = $("#minha-empresa input").val();
+   
+    function salvarInformacoes() {
 
-        // Aqui você pode enviar os dados para o servidor ou fazer o que for necessário com eles
-        // Por enquanto, apenas exibindo no console para demonstração
-        console.log("Email: " + email);
-        console.log("Contato: " + contato);
-        console.log("Endereço: " + endereco);
-        console.log("Cargo: " + cargo);
-        console.log("Modelo de Contratação: " + modeloContratacao);
-        console.log("Minha Empresa: " + minhaEmpresa);
+        let dadosUsuario = {
+            email: $("#email")[0].value,
+            contato: $("#contato")[0].value,
+            endereco: $("#endereco")[0].value,
+            cargo: $("#cargo")[0].value,
+            modeloContratacao: $("#modeloContratacao")[0].value,
+            minhaEmpresa: $("#minhaEmpresa")[0].value,
+        };
 
-        // Adicione aqui a lógica para enviar os dados para o servidor ou armazená-los localmente.
+        console.log("Dados: ", dadosUsuario);
+   
     }
 
-    // Associar a função ao clique do botão sallet
-    $("#button-save").on("click", salletInformacoes);
-
-    // Outras funções (cancelarAcao(), excluirsuario()) podem ser adicionadas aqui ou em outros lugares do seu script, conforme necessário.
+    $("#button-save").on("click", salvarInformacoes);
 
 });
