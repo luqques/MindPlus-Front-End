542a576740b705fdc016f0be36f70bbcd31232ad
Paola K. <kloch.157@gmail.com>
2023-12-05 20:24:10 -0300

    adicionei funcionalidade aos botoes home (js + jquery)

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -1,3 +1,17 @@
-document.getElementById('treinamentos').addEventListener('click', function() {
-    window.location.href = '/treinamentos.html';
+$(document).ready(function() {
+    $('#treinamentos').click(function() {
+        window.location.href = "./treinamentos.html";
+    });
+
+    $('#acesso').click(function() {
+        window.location.href = "./acesse.html";
+    });
+
+    $('#avalie').click(function() {
+        window.location.href = "./avalie.html";
+    });
+
+    $('#opine').click(function() {
+        window.location.href = "./opine.html";
+    });
 });
