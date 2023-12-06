546bd52e2f273327a44acf74c986ac38748bdbc5
IsabelleVolaco <isabelletvolaco@gmail.com>
2023-12-05 23:24:23 -0300

    juntei com scripts da malu, padronizei código

--- src/meus-colaboradores/meus-colaboradores.js
+++ src/meus-colaboradores/meus-colaboradores.js
@@ -1,20 +1,21 @@
-/* Adicionar Colaborador --------------------------------------------- */
-function validateForm() {
+/* Adicionar Colaborador - MALU --------------------------------------------- */
+function addEmployee() {
 
-  var nameInput = document.getElementById('name');
-  var emailInput = document.getElementById('email');
-  var contactInput = document.getElementById('contact');
-  var addressInput = document.getElementById('address');
-  var officeInput = document.getElementById('office');
-  var companyInput = document.getElementById('company');
-  var passwordInput = document.getElementById('password');
+  let nameInput = document.getElementById('nameAdd');
+  let emailInput = document.getElementById('emailAdd');
+  let contactInput = document.getElementById('contactAdd');
+  let addressInput = document.getElementById('addressAdd');
+  let officeInput = document.getElementById('officeAdd');
+  let companyInput = document.getElementById('companyAdd');
+  /*let passwordInput = document.getElementById('password');*/
 
-  var nameError = document.getElementById('nameError');
-  var emailError = document.getElementById('emailError');
-  var contactError = document.getElementById('contactError');
-  var addressError = document.getElementById('addressError');
-
-  var passwordError = document.getElementById('passwordError');
+  let nameError = document.getElementById('nameErrorAdd');
+  let emailError = document.getElementById('emailErrorAdd');
+  let contactError = document.getElementById('contactErrorAdd');
+  let addressError = document.getElementById('addressErrorAdd');
+  let officeError = document.getElementById('officeErrorAdd');
+  let companyError = document.getElementById('companyErrorAdd');
+ /* let passwordError = document.getElementById('passwordError');*/
 
   if (!nameInput.checkValidity()) {
     nameError.textContent = 'Por favor, preencha este campo.';
@@ -29,47 +30,105 @@ function validateForm() {
   }
 
   if (!contactInput.checkValidity()) {
-    contactError.textContent = 'Por favor, insira um contato.'
+    contactError.textContent = 'Por favor, insira um contato.';
   } else {
     contactError.textContent = '';
   }
 
   if (!addressInput.checkValidity()) {
-    addressError.textContent = 'Por favor, insira um endereço.'
+    addressError.textContent = 'Por favor, insira um endereço.';
   } else {
     addressError.textContent = '';
   }
 
   if (!officeInput.checkValidity()) {
-    officeError.textContent = 'Por favor, insira um cargo.'
+    officeError.textContent = 'Por favor, insira um cargo.';
   } else {
     officeError.textContent = '';
   }
 
   if (!companyInput.checkValidity()) {
-    companyError.textContent = 'Por favor, insira uma empresa.'
+    companyError.textContent = 'Por favor, insira uma empresa.';
   } else {
     companyError.textContent = '';
   }
-
+/*
   if (!passwordInput.checkValidity()) {
-    passwordError.textContent = 'Por favor, insira uma senha.'
+    passwordError.textContent = 'Por favor, insira uma senha.';
+    isValid = false
   } else {
     passwordError.textContent = '';
   }
+  */
 }
 
-/* Remover Colaborador ------------------------------------------------ */
+
+/* Alterar Colaborador - ISA --------------------------------------------- */
+
+function editEmployee() {
+  
+  let emailInput = document.getElementById('emailAlt');
+  let nameInput = document.getElementById('nameAlt');
+  let contactInput = document.getElementById('contactAlt');
+  let addressInput = document.getElementById('addressAlt');
+  let officeInput = document.getElementById('officeAlt');
+  let companyInput = document.getElementById('companyAlt');
+
+  let emailError = document.getElementById('emailErrorAlt');
+  let nameError = document.getElementById('nameErrorAlt');
+  let contactError = document.getElementById('contactErrorAlt');
+  let addressError = document.getElementById('addressErrorAlt');
+  let officeError = document.getElementById('officeErrorAlt');
+  let companyError = document.getElementById('companyErrorAlt');
+
+  if (emailInput.text == null || nameInput.text.lenght < 5) {
+    emailError.textContent = 'Insira um endereço de e-mail válido.';
+  } else {
+    emailError.textContent = '';
+  }
+
+  if (nameInput.text == null || nameInput.text.lenght < 5) {
+    nameError.textContent = 'Preencha este campo.';
+  } else {
+    nameError.textContent = '';
+  }
+
+  if (contactInput.text == null || nameInput.text.lenght < 5) {
+    contactError.textContent = 'Insira um contato.';
+  } else {
+    contactError.textContent = '';
+  }
+
+  if (addressInput.text == null || nameInput.text.lenght < 5) {
+    addressError.textContent = 'Insira um endereço.';
+  } else {
+    addressError.textContent = '';
+  }
+
+  if (officeInput.text == null || nameInput.text.lenght < 5) {
+    officeError.textContent = 'Insira um cargo.';
+  } else {
+    officeError.textContent = '';
+  }
+
+  if (companyInput.text == null || nameInput.text.lenght < 5) {
+    companyError.textContent = 'Insira uma empresa.';
+  } else {
+    companyError.textContent = '';
+  }
+}
+
+
+/* Remover Colaborador - MALU ------------------------------------------------ */
 
 function deleteEmployee() {
-  var email = document.getElementById('email').value;
-  var confirmation = document.getElementById('confirmation').checked;
+
+  let email = document.getElementById('email').value;
+  let confirmation = document.getElementById('confirmation').checked;
 
   if (email && confirmation) {
     alert("Funcionário deletado com sucesso!");
   } else {
-    alert("Por favor, preencha o e-mail e confirme a exclusão.");
+    alert("Por favor, preencha o E-mail e confirme a exclusão.");
   }
 }
-
-/* ------------------------------------------------------------------- */
