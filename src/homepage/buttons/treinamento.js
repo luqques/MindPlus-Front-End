d0693ecf9ea9f485c70c68abb5c247bc27b86477
Paola K. <kloch.157@gmail.com>
2023-12-06 13:12:17 -0300

    ajustes nas pastas

--- src/homepage/buttons/treinamento.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#card1').click(function() {
-        window.location.href = "https://www.geolab.com.br/dicas-de-saude/melhorar-a-saude-e-bem-estar/";
-    });
-
-    $('#card2').click(function() {
-        window.location.href = "https://www.vittude.com/blog/como-lidar-com-a-raiva/?utm_source=blog&utm_medium=author&utm_content=blog_traffic";
-    });
-});
