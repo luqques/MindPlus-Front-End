df6a46ee311d13362debe26fe85471b078391a21
Lucas da Cunha <luques.cunha@gmail.com>
2023-12-06 23:14:27 -0300

    Estilização do botão de voltar

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
-// document.getElementById('treinamentos').addEventListener('click', function () {
-//     window.location.href = '../homepage/conscientizacao.html';
-// });
-=======
-$(document).ready(function() {
-    $('#treinamentos').click(function() {
+$(document).ready(function () {
+    $('#treinamentos').click(function () {
         window.location.href = "./treinamentos.html";
     });
 
-    $('#acesso').click(function() {
+    $('#acesso').click(function () {
         window.location.href = "./acesse.html";
     });
 
-    $('#avalie').click(function() {
+    $('#avalie').click(function () {
         window.location.href = "./avalie.html";
     });
 
-    $('#opine').click(function() {
+    $('#opine').click(function () {
         window.location.href = "./opine.html";
     });
 });
 //  lembrar de criar um botao para voltar em todas as rotas
->>>>>>> 04d8ffb94025cd392936ec9dbf24f49356ffbd9f
